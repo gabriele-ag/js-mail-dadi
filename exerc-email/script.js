f59435e0b64f992d2b7193de1a72b3fe8c80f14c
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-17 17:28:27 +0200

    feat: fix exercise email

--- exerc-email/script.js
+++ exerc-email/script.js
@@ -1,14 +1,19 @@
 const mailUtente = prompt("Scrivi la tua e-mail");
-console.log(mailUtente);
+// console.log(mailUtente);
 
 const listaMail = ["utente1@gmail.com", "utente2@gmail.com", "utente3@gmail.com", "utente4@gmail.com", "utente5@gmail.com", "utente6@gmail.com"];
 
-let accesso = false;
+// let accesso = false;
 
-for (let i = 0; i < listaMail.length && mailUtente === listaMail; i++) {
-    accesso = true;
-  
+for (let i = 0; i < listaMail.length; i++) {
+    const mail = listaMail[i]
+    console.log(mail)
+    if (mailUtente === mail) {
+        console.log("Accesso Effettuato");
+        break;
+    } else {
+        console.log("Accesso Fallito");
+        break;
+    }
 }
-
-console.log(accesso)
 
