eec2bf8ade8bc0befa72c46e0629b38788139a24
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-19 14:25:54 +0200

    feat: checkpoint to exer 4

--- bonus/script.js
+++ bonus/script.js
@@ -27,7 +27,7 @@ console.log(colori)
 
 let numVolte = 0;
 
-for (i = 0; i < colori.length; i++) {
+for (let i = 0; i < colori.length; i++) {
     const colNum = colori[i]
     console.log(colNum);
     if (colNum === "rosso") {
@@ -38,3 +38,26 @@ for (i = 0; i < colori.length; i++) {
 }
 
 console.log(`La soluzione dell'esercizio 2 è ${numVolte}`)
+
+
+// ESERCIZIO N 3
+
+const input = [1, 2, 3, 4];
+console.log(input)
+
+const inputRev = []
+
+for (let i = input.length - 1; i >= 0; i--) {
+    const numRev = input[i]
+    inputRev.push(numRev)
+    // console.log(inputRev)
+
+}
+console.log(inputRev)
+console.log(`La soluzione dell'esercizio 3 è ${inputRev}`)
+
+
+// ESERCIZIO N 4
+
+const numeriN4 = [45, 2, 89, 3, 22]
+
