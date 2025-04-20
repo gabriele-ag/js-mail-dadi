4704af45dbddd344f378f6b5bb1ed101b741ac6d
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-20 09:50:32 +0200

    feat: checkpoint exer n 9

--- bonus/script.js
+++ bonus/script.js
@@ -1,5 +1,3 @@
-console.log("ciao")
-
 // ESERCIZIO N 1
 
 const numeri = [2, 5, 8, 3, 10, 7];
@@ -17,6 +15,7 @@ for (let i = 0; i < numeri.length; i++) {
 }
 
 console.log(`La soluzione dell'esercizio 1 è ${somma}`)
+console.log("--------------------------------")
 
 
 // ESERCIZIO N 2
@@ -38,6 +37,7 @@ for (let i = 0; i < colori.length; i++) {
 }
 
 console.log(`La soluzione dell'esercizio 2 è ${numVolte}`)
+console.log("--------------------------------")
 
 
 // ESERCIZIO N 3
@@ -55,6 +55,7 @@ for (let i = input.length - 1; i >= 0; i--) {
 }
 console.log(inputRev)
 console.log(`La soluzione dell'esercizio 3 è ${inputRev}`)
+console.log("--------------------------------")
 
 
 // ESERCIZIO N 4 (DA FARE)
@@ -66,6 +67,8 @@ for (let i = 0; i < numeriN4.length; i++) {
     console.log(numN4)
 }
 
+
+console.log("--------------------------------")
 
 
 // ESERCIZIO N 5
@@ -86,6 +89,63 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 console.log(sommaArr);
+console.log(`La soluzione dell'esercizio 5 è ${sommaArr}`)
+console.log("--------------------------------")
+
 
 // ESERCIZIO N 6
 
+const numN6 = [5, 12, 3, 20, 8];
+let min = numN6[0]
+let max = 0;
+
+for (let i = 0; i < numN6.length; i++) {
+    if (numN6[i] < min) {
+        min = numN6[i]
+    } else if (numN6[i] > max) {
+        max = numN6[i]
+    }
+    
+}
+
+const result = max - min
+console.log(result);
+console.log(`La soluzione dell'esercizio 5 è ${result}`);
+console.log("--------------------------------")
+
+
+// ESERCIZIO N 7
+
+const flags = [true, false, true, true, false];
+
+let numVolte2 = 0
+
+for (let i = 0; i < flags.length; i++) {
+    if (flags[i] === true) {
+        numVolte2++
+    }
+}
+
+console.log(numVolte2);
+console.log(`La soluzione dell'esercizio 7 è ${numVolte2}`);
+console.log("--------------------------------");
+
+
+// ESERCIZIO N 8
+
+const numeriN8 = [1, 2, 3, 4];
+
+const doppio =[]
+
+for (let i = 0; i < numeriN8.length; i++) {
+    const nDoppio = numeriN8[i] * 2;
+    console.log(nDoppio)
+    doppio.push(nDoppio)
+}
+
+console.log(doppio)
+console.log(`La soluzione dell'esercizio 8 è ${doppio}`);
+console.log("--------------------------------");
+
+
+// ESERCIZIO N 9
